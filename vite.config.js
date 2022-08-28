a474c9ed573e0ab8b09575732303049aa6d5683f
ChipoHex <111188841+ChipoHex@users.noreply.github.com>
2022-08-28 16:46:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
-
 
 import shopifyConfig from './shopify.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen(), netlifyPlugin()],
-  resolve: {
-    alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
-  },
+  plugins: [hydrogen(shopifyConfig)],
   optimizeDeps: {include: ['@headlessui/react']},
   test: {
     globals: true,
